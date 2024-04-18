a4beddbfa19737cf85ecb3b28c2fce4d7120ddc8
Nick <hello@swiftful-thinking.com>
2024-04-18 11:32:41 -0500

    Initial Commit

---
+++ SwiftfulFirebasePackagesExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftfulFirebasePackagesExample
+//
+//  Created by Nick Sarno on 4/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftfulFirebasePackagesExample/SwiftfulFirebasePackagesExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulFirebasePackagesExampleApp.swift
+//  SwiftfulFirebasePackagesExample
+//
+//  Created by Nick Sarno on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulFirebasePackagesExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
